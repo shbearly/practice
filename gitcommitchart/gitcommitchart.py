7b038bb1dce0013fc3e5895cf0fab94cbb0ed437
Yan Li <yan.li1@technicolor.com>
2019-01-08 18:14:07 -0500

    add more chart

--- gitcommitchart/gitcommitchart.py
+++ gitcommitchart/gitcommitchart.py
@@ -2,7 +2,7 @@
 import os
 
 ''' output gittxt first '''
-commitFile="/mnt/40g/python/gitcommitchart/commits.txt"
+commitFile="/mnt/40g/python/practice/gitcommitchart/commits.txt"
 os.system('pushd /mnt/40g/usgv2/intel_usg; git --no-pager log --pretty="format:%h/%an/%ad" > ' + commitFile +'; pushd')
 
 '''parse the file'''
@@ -24,8 +24,96 @@ readFile(commitFile)
 
 ldbs=sorted(dbs.items(), key=lambda i:i[1], reverse=True)
 
-for i in ldbs:
-	print(i[1], i[0])
 	
+'''Generate the html file'''
+header='''
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+<script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"> </script>
+
+</head>
+'''
+
+body1='''
+<body><h1>It works!</h1>
+<p>This is the default web page for this server.</p>
+<p>The web server software is running but no content has been added, yet.</p>
+'''
+
+chart1='''
+<div class="ct-chart ct-perfect-fourth"></div>
+'''
+
+name=[]
+commit=[]
+max= 10
+for i in ldbs:
+	name.append(i[0])
+	commit.append(i[1])
+	max -= 1
+	if max == 0:
+		break
+
+chart2='''
+<script>
+var data1 = {
+  labels: ''' + str(name) + ''',
+  series: [
+	''' + str(commit) + '''
+  ]
+};
+
+var options1 = {
+  width: 900,
+  height: 700
+};
+
+new Chartist.Line('.ct-chart', data1, options1);
+
+</script>
+
+
+<p>The web server software is running but no content has been added, yet.</p>
+
+<div class="ct-chart2 ct-perfect-fourth"></div>
+
+<script>
+var data2 = {
+  labels: ''' + str(name) + ''',
+  series: [
+	''' + str(commit) + '''
+  ]
+};
+
+options2 = {
+  seriesBarDistance: 400,
+  reverseData: true,
+  horizontalBars: true,
+  axisY: {
+    offset: 70
+  }
+}
+
+
+new Chartist.Bar('.ct-chart2', data2, options2 );
+</script>
+
+'''
+
+body2='''
+</body>
+</html>
+'''
+
+f = open("./index.html","w+")
+if f is not None:
+	f.write(header)
+	f.write(body1)
+	f.write(chart1)
+	f.write(chart2)
+	f.write(body2)
+
+
 
 
